dd43e5f5f84d9820e895e832e707ef0bae460824
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-02-27 06:46:28 -0800

    Add classes to service config

--- src/Module.php
+++ src/Module.php
@@ -1,6 +1,7 @@
 <?php
 namespace LeoGalleguillos\Business;
 
+use LeoGalleguillos\Business\Model\Factory as BusinessFactory;
 use LeoGalleguillos\Business\Model\Service as BusinessService;
 use LeoGalleguillos\Business\Model\Table as BusinessTable;
 use LeoGalleguillos\Business\View\Helper as BusinessHelper;
@@ -24,10 +25,21 @@ class Module
     {
         return [
             'factories' => [
+                BusinessFactory\Business::class => function ($serviceManager) {
+                    return new BusinessFactory\Business(
+                        $serviceManager->get(BusinessTable\Business::class)
+                    );
+                },
                 BusinessService\Create::class => function ($serviceManager) {
                     return new BusinessService\Create(
                         $serviceManager->get(BusinessTable\Business::class),
                         $serviceManager->get(StringService\UrlFriendly::class)
+                    );
+                },
+                BusinessService\Newest::class => function ($serviceManager) {
+                    return new BusinessService\Newest(
+                        $serviceManager->get(BusinessFactory\Business::class),
+                        $serviceManager->get(BusinessTable\Business::class)
                     );
                 },
                 BusinessTable\Business::class => function ($serviceManager) {
