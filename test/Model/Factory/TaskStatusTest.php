6070d1978acc775eebbd6a75ec49b16865eacbe6
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-03-19 14:18:23 -0700

    Write preliminary test for task status factory

---
+++ test/Model/Factory/TaskStatusTest.php
@@ -0,0 +1,29 @@
+<?php
+namespace LeoGalleguillos\BusinessTest\Model\Factory;
+
+use LeoGalleguillos\Business\Model\Entity as BusinessEntity;
+use LeoGalleguillos\Business\Model\Factory as BusinessFactory;
+use LeoGalleguillos\Business\Model\Table as BusinessTable;
+use LeoGalleguillos\Flash\Model\Service as FlashService;
+use PHPUnit\Framework\TestCase;
+
+class TaskStatusTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->taskStatusTableMock = $this->createMock(
+            BusinessTable\TaskStatus::class
+        );
+        $this->taskStatusFactory = new BusinessFactory\TaskStatus(
+            $this->taskStatusTableMock
+        );
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            BusinessFactory\TaskStatus::class,
+            $this->taskStatusFactory
+        );
+    }
+}
