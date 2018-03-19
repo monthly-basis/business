ac79d9f025e449e6ff99eb7ac4c4cd74dc4ba03f
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-03-19 13:04:25 -0700

    Add task_status_id column to task table

--- sql/leogalle_test/task/create.sql
+++ sql/leogalle_test/task/create.sql
@@ -1,8 +1,9 @@
 CREATE TABLE `task` (
     `task_id` int(10) unsigned not null auto_increment,
-    `business_id` int(10) not null,
+    `business_id` int(10) unsigned not null,
     `summary` varchar(255) not null,
     `description` text not null,
+    `task_status_id` int(10) unsigned default null,
     `views` int unsigned not null default 0,
     `created` datetime not null,
     PRIMARY KEY (`task_id`),
