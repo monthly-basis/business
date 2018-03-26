01817bcf16b8c33bf145cb28866465445efcb315
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-03-26 10:34:25 -0700

    Write preliminary test for get service

---
+++ test/Model/Service/TaskStatus/TaskStatuses/GetTest.php
@@ -0,0 +1,33 @@
+<?php
+namespace LeoGalleguillos\BusinessTest\Model\Service\TaskStatus\TaskStatuses;
+
+use LeoGalleguillos\Business\Model\Entity as BusinessEntity;
+use LeoGalleguillos\Business\Model\Factory as BusinessFactory;
+use LeoGalleguillos\Business\Model\Service as BusinessService;
+use LeoGalleguillos\Business\Model\Table as BusinessTable;
+use PHPUnit\Framework\TestCase;
+
+class GetTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->taskStatusFactoryMock = $this->createMock(
+            BusinessFactory\TaskStatus::class
+        );
+        $this->taskStatusTableMock = $this->createMock(
+            BusinessTable\TaskStatus::class
+        );
+        $this->getService = new BusinessService\TaskStatus\TaskStatuses\Get(
+            $this->taskStatusFactoryMock,
+            $this->taskStatusTableMock
+        );
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            BusinessService\TaskStatus\TaskStatuses\Get::class,
+            $this->getService
+        );
+    }
+}
