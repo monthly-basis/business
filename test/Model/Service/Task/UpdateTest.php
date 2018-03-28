1b9586d08c1c421d88e9b133a209ff6107dff2ce
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-03-27 18:11:22 -0700

    Write preliminary test for update method

--- test/Model/Service/Task/UpdateTest.php
+++ test/Model/Service/Task/UpdateTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace LeoGalleguillos\BusinessTest\Service\Task;
 
+use Exception;
 use LeoGalleguillos\Business\Model\Entity as BusinessEntity;
 use LeoGalleguillos\Business\Model\Service as BusinessService;
 use LeoGalleguillos\Business\Model\Table as BusinessTable;
@@ -30,5 +31,21 @@ class UpdateTest extends TestCase
             BusinessService\Task\Update::class,
             $this->updateService
         );
+    }
+
+    public function testUpdate()
+    {
+        $taskEntity = new BusinessEntity\Task();
+
+        $_POST = [];
+        try {
+            $this->updateService->update($taskEntity);
+            $this->fail();
+        } catch (Exception $exception) {
+            $this->assertSame(
+                'Invalid form input.',
+                $exception->getMessage()
+            );
+        }
     }
 }
