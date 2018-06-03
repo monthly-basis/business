db1b2057fc1a351c78a652130c1d7b240f088016
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-06-03 00:15:42 +0000

    Add order column to task_status table

--- sql/leogalle_test/task_status/create.sql
+++ sql/leogalle_test/task_status/create.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `task_status` (
     `task_status_id` int(10) unsigned not null auto_increment,
-    `name` varchar(255) not null,
+    `name` varchar(255) NOT NULL,
+    `order` int(3) DEFAULT NULL,
     PRIMARY KEY (`task_status_id`)
 ) charset=utf8;
