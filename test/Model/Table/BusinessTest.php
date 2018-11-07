a82e2423deda27e54e14693deb92e2896cacb98e
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-11-07 22:21:48 +0000

    Add and start to use test package

--- test/Model/Table/BusinessTest.php
+++ test/Model/Table/BusinessTest.php
@@ -3,38 +3,16 @@ namespace LeoGalleguillos\BusinessTest\Model\Table;
 
 use Generator;
 use LeoGalleguillos\Business\Model\Table as BusinessTable;
-use LeoGalleguillos\BusinessTest\TableTestCase;
-use Zend\Db\Adapter\Adapter;
-use PHPUnit\Framework\TestCase;
+use LeoGalleguillos\Test\TableTestCase;
 
 class BusinessTest extends TableTestCase
 {
-    /**
-     * @var string
-     */
-    protected $sqlPath = __DIR__ . '/../../..' . '/sql/leogalle_test/business/';
-
     protected function setUp()
     {
-        $configArray     = require(__DIR__ . '/../../../config/autoload/local.php');
-        $configArray     = $configArray['db']['adapters']['leogalle_test'];
-        $this->adapter   = new Adapter($configArray);
-        $this->businessTable = new BusinessTable\Business($this->adapter);
+        $this->businessTable = new BusinessTable\Business($this->getAdapter());
 
-        $this->dropTable();
-        $this->createTable();
-    }
-
-    protected function dropTable()
-    {
-        $sql = file_get_contents($this->sqlPath . 'drop.sql');
-        $result = $this->adapter->query($sql)->execute();
-    }
-
-    protected function createTable()
-    {
-        $sql = file_get_contents($this->sqlPath . 'create.sql');
-        $result = $this->adapter->query($sql)->execute();
+        $this->dropTable('business');
+        $this->createTable('business');
     }
 
     public function testInitialize()
